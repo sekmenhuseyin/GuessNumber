feb48aead2a2b2547472f2e2bce0ae01029e0f96
huseyin <hus_@yahoo.com>
2015-03-03 14:39:55 +0200

    03.03.2015: first working copy

---
+++ GuessNumber/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class frmMain
+    Dim Aklimda, Level, Defa As Integer
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Level = 1 : Aklimda = Rnd() * (4 * Level) + 1 : Defa = 0
+        Label2.Text = "Write a number between 1 and " & (4 * Level) + 1
+    End Sub
+    Private Sub TextBox1_KeyDown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If TextBox1.Text = "" Then Exit Sub
+        If IsNumeric(TextBox1.Text) = False Then Label1.Text = "Write a number please!" : Exit Sub
+        If e.KeyCode = Keys.Enter Then
+            Defa += 1
+            If TextBox1.Text = Aklimda Then
+                Label1.Text = "Bravo!" + vbCrLf + "You have guessed " + Defa.ToString + " times!" + vbCrLf + "New game started"
+                Aklimda = Rnd() * (4 * Level) + 1 : Level += 1 : TextBox1.Text = "" : Defa = 0
+                Label2.Text = "Write a number between 1 and " & (4 * Level) + 1
+            ElseIf Int(TextBox1.Text) > Aklimda Then
+                Label1.Text = "write a smaller value" : TextBox1.SelectAll()
+            ElseIf Int(TextBox1.Text) < Aklimda Then
+                Label1.Text = "write a bigger value" : TextBox1.SelectAll()
+            End If
+        End If
+    End Sub
+End Class
---
+++ GuessNumber/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Guess the Number")> 
+<Assembly: AssemblyDescription("Guess the Number")> 
+<Assembly: AssemblyCompany("Huseyin Sekmenoglu")> 
+<Assembly: AssemblyProduct("GuessNumber")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("Huseyin Sekmenoglu")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6c71000f-a4b7-4544-85bc-dc3df9fd5c04")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.0")> 
+<Assembly: AssemblyFileVersion("0.0.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("tr")> 
